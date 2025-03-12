d5187dd7a5a73474bf9ae81506d09f2a886e62e1
iriaagullo <iriaagullo22@gmail.com>
2025-03-12 20:44:44 +0100

    cambios

--- src/main/python/programmingtheiot/data/DataUtil.py
+++ src/main/python/programmingtheiot/data/DataUtil.py
@@ -7,6 +7,9 @@
 # and designed to be modified by the student as needed.
 #
 
+import json
+import logging
+from decimal import Decimal
 from json import JSONEncoder
 
 from programmingtheiot.data.ActuatorData import ActuatorData
@@ -20,24 +23,108 @@ class DataUtil():
 	"""
 
 	def __init__(self, encodeToUtf8 = False):
+		self.encodeToUtf8=encodeToUtf8
+		logging.info("Created DataUtil instance.")
+		
 		pass
 	
-	def actuatorDataToJson(self, data: ActuatorData = None):
+	def actuatorDataToJson(self, data: ActuatorData = None, useDecForFloat: bool = False ):
+		if not data:
+			logging.debug("ActuatorData is null. Returning empty string.")
+			return""
+
+		jsonData=self._generateJsonData(obj=data,useDecForFloat=False)
+		return jsonData
 		pass
 	
-	def sensorDataToJson(self, data: SensorData = None):
+	def sensorDataToJson(self, data: SensorData = None, useDecForFloat: bool = False):
+		if not data:
+			logging.debug("SensorData is null. Returning empty string.")
+			return ""
+
+		jsonData = self._generateJsonData(obj=data, useDecForFloat=useDecForFloat)
+		return jsonData
+		
 		pass
 
-	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None):
+	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None, useDecForFloat: bool = False):
+		if not data:
+			logging.debug("SystemPerformanceData is null. Returning empty string.")
+			return ""
+
+		jsonData = self._generateJsonData(obj=data, useDecForFloat=useDecForFloat)
+		return jsonData
 		pass
 	
-	def jsonToActuatorData(self, jsonData: str = None):
+	def jsonToActuatorData(self, jsonData: str = None, useDecForFloat: bool = False):
+		if not jsonData:
+			logging.warning("JSON data is empty or null. Returning null.")
+			return None
+
+		jsonStruct=self._formatDataAndLoadDictionary(jsonData,useDecForFloat=useDecForFloat)
+		ad=ActuatorData()
+		self._updateIotData(jsonStruct,ad)
+		return ad
+		pass
+
+	def _formatDataAndLoadDictionary(self,jsonData:str,useDecForFloat:bool=False)->dict:
+		jsonData=jsonData.replace("\'","\"").replace('False','false').replace('True','true')
+
+		jsonStruct=None
+
+		if useDecForFloat:
+			jsonStruct=json.loads(jsonData,parse_float=Decimal)
+		else:
+			jsonStruct=json.loads(jsonData)
+
+		return jsonStruct
+		pass
+
+	def _generateJsonData(self,obj,useDecForFloat:bool=False)->str:
+		jsonData=None
+
+		if self.encodeToUtf8:
+			jsonData=json.dumps(obj,cls=JsonDataEncoder).encode('utf8')
+		else:
+			jsonData=json.dumps(obj,cls=JsonDataEncoder,indent=4)
+
+		if jsonData:
+			jsonData=jsonData.replace("\'","\"").replace('False','false').replace('True','true')
+
+		return jsonData
+		pass
+
+	def _updateIotData(self,jsonStruct,obj):
+		varStruct=vars(obj)
+
+		for key in jsonStruct:
+			if key in varStruct:
+				setattr(obj,key,jsonStruct[key])
+			else:
+				logging.warn("JSON data contains key not mappable to object: %s",key)
+		
+		pass
+
+	def jsonToSensorData(self, jsonData: str = None, useDecForFloat: bool = False):
+		if not jsonData:
+			logging.warning("JSON data is empty or null. Returning null.")
+			return None
+
+		jsonStruct = self._formatDataAndLoadDictionary(jsonData, useDecForFloat=useDecForFloat)
+		sd = SensorData()
+		self._updateIotData(jsonStruct, sd)
+		return sd
 		pass
 	
-	def jsonToSensorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSystemPerformanceData(self, jsonData: str = None):
+	def jsonToSystemPerformanceData(self, jsonData: str = None, useDecForFloat: bool = False):
+		if not jsonData:
+			logging.warning("JSON data is empty or null. Returning null.")
+			return None
+
+		jsonStruct = self._formatDataAndLoadDictionary(jsonData, useDecForFloat=useDecForFloat)
+		spd = SystemPerformanceData()
+		self._updateIotData(jsonStruct, spd)
+		return spd
 		pass
 	
 class JsonDataEncoder(JSONEncoder):
@@ -47,5 +134,5 @@ class JsonDataEncoder(JSONEncoder):
 	
 	"""
 	def default(self, o):
+		
 		return o.__dict__
-	
